44088ac8d5acbf4724648f656e88dc8a0a9cdd23
Jeremy Shank <jeremyshank@bmss.com>
2021-04-08 13:38:17 -0500

    added missed option for override

--- routes/bingo/override.js
+++ routes/bingo/override.js
@@ -28,14 +28,13 @@ const BASE = (req, res) => {
         let missedUsers = []
         let pool = await sql.connect(config.datawarehouse)
         let data = await pool.request()
-            .query(`SELECT BC.BingoUser
+            .query(`SELECT BC.BingoUser, SUM(B.BingoMissed) AS Missed
             FROM dbo.Bingo B
             INNER JOIN dbo.BingoCards BC ON BC.BingoCard = B.BingoCard
-            WHERE CONVERT(DATE, BingoDate) = CONVERT(DATE, GETDATE()) AND BingoNumber = 0`)
+            WHERE CONVERT(DATE, BingoDate) = CONVERT(DATE, GETDATE()) AND BingoNumber = 0
+            GROUP BY BC.BingoUser`)
         
-        data.recordset.forEach(user => {
-            missedUsers.push(user.BingoUser)
-        })
+        missedUsers = data.recordset
         pool.close()
         return missedUsers
     }
@@ -47,9 +46,9 @@ const BASE = (req, res) => {
                 let pool = await sql.connect(config.engine)
                 for (let i = 0; i < staff.length; i++) {
                     let data = await pool.request()
-                        .input('staff', sql.Int, staff[i])
+                        .input('staff', sql.Int, staff[i].BingoUser)
                         .query('SELECT StaffIndex, StaffName FROM dbo.tblStaff WHERE StaffIndex = @staff')
-                    returnArr.push(data.recordset[0])
+                    returnArr.push({StaffIndex: data.recordset[0].StaffIndex, StaffName: data.recordset[0].StaffName, BingoMissed: staff[i].BingoMissed})
                 }
                 pool.close()
                 return returnArr
